153379c3e81a24a52158d946de65fb5987f46cff
TranTrungPhong <achinpo1999@gmail.com>
2025-01-20 02:09:37 +0700

    Revert "code"
    This reverts commit 6faaccfd3276c55dd78a2b894d1a29465ada7e16.

--- src/main/java/com/example/ontapktnc2/HelloServlet.java
+++ src/main/java/com/example/ontapktnc2/HelloServlet.java
@@ -26,9 +26,6 @@ public class HelloServlet extends HttpServlet {
         System.out.println("yeu em");
 
         System.out.println("merge");
-
-
-        System.out.println("anh yeu em lam code ak");
     }
 
     public void destroy() {
